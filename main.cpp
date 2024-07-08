fa35f533fdcc853a9b68e5756a4073fe1d6a4d77
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-07-08 22:17:08 +0300

    create dir

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "arith.hpp"
+
+int main(int argc, char** argv, char** env) {
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
---
+++ src/arith.cpp
@@ -0,0 +1,1 @@
+#include "arith.hpp"
