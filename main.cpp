7ba96546b323c8b35d73a76bc451d381d743c7ce
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-07-09 22:50:17 +0300

    check

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv, char **env) {
   int res3 = arith::mul(res1, res2);
   int res4 = arith::div(res3, res3);
 
-  std::cout << "res1 = " << res1 << '\n';
+                    std::cout << "res1 = " << res1 << '\n';
   std::cout << "res2 = " << res2 << '\n';
   std::cout << "res3 = " << res3 << '\n';
   std::cout << "res4 = " << res4 << '\n';
