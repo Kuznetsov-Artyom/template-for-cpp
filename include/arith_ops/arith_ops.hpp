1c96f491aaa2a6e62f8009293f5833547d425f43
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-07-27 10:45:13 +0300

    add tests

--- include/arith_ops/arith_ops.hpp
+++ include/arith_ops/arith_ops.hpp
@@ -2,10 +2,10 @@
 #define _ARITH_OPS_HPP_
 
 namespace arith {
-constexpr int add(int a, int b) noexcept;
-constexpr int sub(int a, int b) noexcept;
-constexpr int mul(int a, int b) noexcept;
-constexpr int div(int a, int b) noexcept;
+int add(int a, int b) noexcept;
+int sub(int a, int b) noexcept;
+int mul(int a, int b) noexcept;
+int div(int a, int b) noexcept;
 }  // namespace arith
 
 #endif  // _ARITH_OPS_HPP_
--- lib/arith_ops/arith_ops.cpp
+++ lib/arith_ops/arith_ops.cpp
@@ -1,8 +1,8 @@
 #include "arith_ops.hpp"
 
 namespace arith {
-constexpr int add(int a, int b) noexcept { return a + b; }
-constexpr int sub(int a, int b) noexcept { return a - b; }
-constexpr int mul(int a, int b) noexcept { return a * b; }
-constexpr int div(int a, int b) noexcept { return a / b; }
+int add(int a, int b) noexcept { return a + b; }
+int sub(int a, int b) noexcept { return a - b; }
+int mul(int a, int b) noexcept { return a * b; }
+int div(int a, int b) noexcept { return a / b; }
 }  // namespace arith
