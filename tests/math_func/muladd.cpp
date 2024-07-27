7d8caa5372138071ead03447e2d34d1f7aded9f3
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-07-27 15:43:42 +0300

    upd

---
+++ tests/math_func/muladd.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "muladd.hpp"
+
+TEST(Test_case_muladd, test_1) {
+  constexpr int a = 5;
+  constexpr int b = 5;
+  constexpr int c = 5;
+  ASSERT_EQ(30, func::muladd(a, b, c));
+}
+
+TEST(Test_case_muladd, test_2) {
+  constexpr int a = 5;
+  constexpr int b = 5;
+  constexpr int c = -25;
+  ASSERT_EQ(0, func::muladd(a, b, c));
+}
