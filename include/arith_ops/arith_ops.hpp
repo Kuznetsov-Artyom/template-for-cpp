ce852b931a50a85116206bb554d9a3780179bb05
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-07-27 09:43:47 +0300

    upd

--- include/arith_ops/arith_ops.hpp
+++ include/arith_ops/arith_ops.hpp
@@ -1,11 +1,11 @@
-#ifndef _ARITH_HPP_
-#define _ARITH_HPP_
+#ifndef _ARITH_OPS_HPP_
+#define _ARITH_OPS_HPP_
 
 namespace arith {
-int add(int a, int b);
-int sub(int a, int b);
-int mul(int a, int b);
-int div(int a, int b);
+constexpr int add(int a, int b) noexcept;
+constexpr int sub(int a, int b) noexcept;
+constexpr int mul(int a, int b) noexcept;
+constexpr int div(int a, int b) noexcept;
 }  // namespace arith
 
-#endif  // _ARITH_HPP_
+#endif  // _ARITH_OPS_HPP_
--- lib/arith_ops/arith_ops.cpp
+++ lib/arith_ops/arith_ops.cpp
@@ -1,8 +1,8 @@
-#include "arith.hpp"
+#include "arith_ops.hpp"
 
 namespace arith {
-int add(int a, int b) { return a + b; }
-int sub(int a, int b) { return a - b; }
-int mul(int a, int b) { return a * b; }
-int div(int a, int b) { return a / b; }
+constexpr int add(int a, int b) noexcept { return a + b; }
+constexpr int sub(int a, int b) noexcept { return a - b; }
+constexpr int mul(int a, int b) noexcept { return a * b; }
+constexpr int div(int a, int b) noexcept { return a / b; }
 }  // namespace arith
