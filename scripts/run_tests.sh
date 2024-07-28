17a4af07dd2fc847f45b0fd00c594e24fc4ecbd4
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-07-28 11:22:09 +0300

    fix

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./build/bin/tests/arith_ops_test --gtest_repeat=10
-./build/bin/tests/math_func_test --gtest_repeat=10
+./build/bin/arith_ops_test --gtest_repeat=10
+./build/bin/math_func_test --gtest_repeat=10
