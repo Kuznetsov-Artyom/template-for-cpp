1239c9d2cedc392b22e942da08af14ca6a397415
Kuznetsov-Artyom <110616662+Kuznetsov-Artyom@users.noreply.github.com>
2024-07-28 10:43:12 +0300

    Add run tests in CI (#12)
    * test
    
    * try
    
    * upd
    
    * upd

---
+++ scripts/run_tests.bat
@@ -0,0 +1,4 @@
+@echo off
+
+build\bin\tests\arith_ops_test.exe --gtest_repeat=10
+build\bin\tests\math_func_test.exe --gtest_repeat=10
---
+++ scripts/run_tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./build/bin/tests/arith_ops_test --gtest_repeat=10
+./build/bin/tests/math_func_test --gtest_repeat=10
