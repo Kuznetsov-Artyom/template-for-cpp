0a6c7e712d2f6192f3059bfc2ecb0aa37f2424eb
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-07-28 08:13:05 +0300

    fix style

--- lib/math_func/muladd.cpp
+++ lib/math_func/muladd.cpp
@@ -1,7 +1,7 @@
 #include "math_func/muladd.hpp"
-#include "math_func/factorial.hpp"
 
 #include "arith_ops/arith_ops.hpp"
+#include "math_func/factorial.hpp"
 
 namespace func {
 int muladd(int a, int b, int c) noexcept {
