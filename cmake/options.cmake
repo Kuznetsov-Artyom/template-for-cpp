dbc902f003bb0a31cd7f2dadd8d3b4b42885b013
Kuznetsov-Artyom <110616662+Kuznetsov-Artyom@users.noreply.github.com>
2025-01-18 11:47:32 +0300

    Added cmake file for options (#20)
    * add cmake file for options
    
    * deleted '\'

---
+++ cmake/options.cmake
@@ -0,0 +1,13 @@
+option(ENABLE_TESTS "Enable testing" OFF)
+option(CCACHE_BUILD "Building using ccache" OFF)
+
+if (CCACHE_BUILD)
+    find_program(CCACHE_FOUND ccache)
+    if (CCACHE_FOUND)
+        message("-- Found ccache - FOUND")
+    else()
+        message(FATAL_ERROR "-- Found ccache - NOT FOUND")
+    endif()
+    set(CMAKE_C_COMPILER_LAUNCHER ccache)
+    set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
+endif()
